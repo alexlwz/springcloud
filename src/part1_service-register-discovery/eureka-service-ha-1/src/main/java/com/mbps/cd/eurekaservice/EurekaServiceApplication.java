8164e913cff3260e9c4074139ee7beb54247ebff
cffex <liwenzhong1991@126.com>
2019-09-11 16:55:33 +0800

    test

--- src/part1_service-register-discovery/eureka-service-ha-1/src/main/java/com/mbps/cd/eurekaservice/EurekaServiceApplication.java
+++ src/part1_service-register-discovery/eureka-service-ha-1/src/main/java/com/mbps/cd/eurekaservice/EurekaServiceApplication.java
@@ -12,6 +12,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServiceApplication.class, args);
+		SpringApplication.run( EurekaServiceApplication.class, args);
 	}
 }
